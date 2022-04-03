989691b199c3d0aaab424a4b2509c6f71010aa37
Billy Chan <billychan223@gmail.com>
2022-04-03 11:51:41 +0800

    fix: build

--- components/Booking.tsx
+++ components/Booking.tsx
@@ -53,6 +53,7 @@ export default function Booking({ booking, user }: BookingProps) {
       <TimeRangeInput
         mt={"sm"}
         value={[new Date(booking?.data?.start), new Date(booking?.data?.end)]}
+        // @ts-ignore
         readOnly
         style={{ maxWidth: "150px" }}
       />
@@ -74,37 +75,5 @@ export default function Booking({ booking, user }: BookingProps) {
         </Link>
       )}
     </Card>
-  );
-  return (
-    <div className="bg-gray-100 p-4 rounded-md my-2 shadow-lg">
-      <div className="flex items-center justify-between mb-2">
-        <div className="flex items-center">
-          <h2 className="text-xl text-gray-800 font-bold">
-            Room - {booking.data.room}
-          </h2>
-          {timePassed && (
-            <Badge
-              className="ml-2"
-              variant="gradient"
-              gradient={{ from: "#ed6ea0", to: "#ec8c69", deg: 35 }}
-            >
-              Expired Booking
-            </Badge>
-          )}
-        </div>
-
-        <TimeRangeInput
-          value={[new Date(booking?.data?.start), new Date(booking?.data?.end)]}
-          readOnly
-        />
-      </div>
-      <h3 className="text-md text-gray-800 font-bold mb-2">Remarks</h3>
-      <p className="text-gray-900 mb-4">{booking.data.remarks}</p>
-      {user?.sub === booking.data.userId && !timePassed && (
-        <Link href={`/edit/${booking.id}`}>
-          <a className="text-gray-800 mr-2">Edit</a>
-        </Link>
-      )}
-    </div>
   );
 }
